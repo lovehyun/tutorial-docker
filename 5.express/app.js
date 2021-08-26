59b80a25fecc08d84b0e4b581b41e70a7a8ca258
Su-Hyun Park <lovehyun@hotmail.com>
2021-08-27 01:50:57 +0900

    [DEV] 호스트네임 출력 로그 추가

--- 5.express/app.js
+++ 5.express/app.js
@@ -10,11 +10,12 @@ app.get('/', function (req,res) {
     ip = req.connection.remoteAddress;
     // ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
-    console.log("Received request from " + ip);
-    res.send("<h2>Welcome to " + os.hostname() + "</h2>");
+    console.log('Received request from ' + ip);
+    res.send('<h2>Welcome to ' + os.hostname() + '</h2>');
     // res.send("<h1>Welcome to " + os.hostname() + "</h1>");
 });
 
 app.listen(8000, () => 
     console.log('Express is ready at http://localhost:8000')
+    // console.log('Express is ready at http://%s:8000', os.hostname())
 );
