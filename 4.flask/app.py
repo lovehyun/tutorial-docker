33af54d976fbe3cbd7bb55d55146a9b05b0e1a4a
Su-Hyun Park <lovehyun@hotmail.com>
2022-01-28 02:14:56 +0900

    [DEV] ProxyFix 추가 및 불필요한 디버그 제거

--- 4.flask/app.py
+++ 4.flask/app.py
@@ -1,7 +1,9 @@
 import os
 from flask import Flask, render_template
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
 color = os.environ.get('APP_COLOR')
 
@@ -10,4 +12,4 @@ def main():
     return render_template('hello.html', color=color)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
