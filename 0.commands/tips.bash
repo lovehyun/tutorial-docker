1e83db1c676a33f38a9933867f41abcbc6d606ae
Su-Hyun Park <lovehyun@hotmail.com>
2022-10-02 22:33:35 +0900

    [DEV] dockerhub 에서 tag 가져오기 버전2 - 버전1이 최근부터 410 Gone 으로 바뀜

--- 0.commands/tips.bash
+++ 0.commands/tips.bash
@@ -1,6 +1,7 @@
 function docker-taglist {
   if [ ! -z $1 ]; then
-    curl -s https://registry.hub.docker.com/v1/repositories/$1/tags | sed "s/,/\n/g" | grep name | cut -d '"' -f 4
+    # curl -s https://registry.hub.docker.com/v1/repositories/$1/tags | sed "s/,/\n/g" | grep name | cut -d '"' -f 4
+    curl -s https://hub.docker.com/v2/repositories/$1/tags/list | jq
   else
     echo -e "\nusage: docker-taglist [imagename]\n"
   fi
@@ -18,3 +19,26 @@ function docker-container-ip {
     echo -e "\nusage: docker-container-ip [container-name] [container-network]\n"
   fi
 }
+
+function docker-taglist2 () { 
+    local repo=${1} 
+    local page_size=${2:-100} 
+    [ -z "${repo}" ] && echo "Usage: listTags <repoName> [page_size]" 1>&2 && return 1 
+    local base_url="https://registry.hub.docker.com/api/content/v1/repositories/public/library/${repo}/tags" 
+     
+    local page=1 
+    local res=$(curl "${base_url}?page_size=${page_size}&page=${page}" 2>/dev/null) 
+    local tags=$(echo ${res} | jq --raw-output '.results[].name') 
+    local all_tags="${tags}" 
+ 
+    local tag_count=$(echo ${res} | jq '.count')   
+ 
+    ((page_count=(${tag_count}+${page_size}-1)/${page_size}))  # ceil(tag_count / page_size) 
+ 
+    for page in $(seq 2 $page_count); do 
+        tags=$(curl "${base_url}?page_size=${page_size}&page=${page}" 2>/dev/null | jq --raw-output '.results[].name') 
+        all_tags="${all_tags}${tags}" 
+    done 
+ 
+    echo "${all_tags}" | sort 
+} 
